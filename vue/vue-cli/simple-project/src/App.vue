9ed27ee88d294a123e1e67a935f40ce0e29d13ab
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-11-09 23:25:52 -0300

    vue-cli

---
+++ vue/vue-cli/simple-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="#app">
+    {{ title }}
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      title:'Hello Vue'
+    }
+  }
+}
+</script>
+
+<style>
+  #app{
+    color: green;
+    font-weight: bold;
+    font-size:3rem;
+    text-align: center;
+  }
+</style>
