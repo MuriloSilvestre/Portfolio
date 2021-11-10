bbf1b6fce6c97d3c8ef6314e8e5886d835d22f95
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-11-10 11:48:57 -0300

    vue-cli-components

---
+++ vue/vue-cli/project-component/src/counter.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="counter">
+        <div>
+            <span>{{ counter }}</span>
+        </div>
+        <div>
+            <button @click="add"> + </button>        
+            <button @click="remove"> - </button>        
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data:()=>{
+        return{
+            counter: 0
+        }
+    },
+    methods:{
+        add(){
+            this.counter++
+        },
+        remove(){
+            this.counter--
+        }
+    }
+}
+</script>
+
+<style>
+    .counter{
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .counter span{
+        font-weight: bold;
+        font-size: 2rem;
+        color: green;
+    }
+    
+    .counter button{
+        background-color: green;
+        border-color: green;
+        color: white;
+        font-weight: bold;
+        font-size: 1rem;
+        margin: 10px 20px;
+        outline: none;
+    }
+</style>
