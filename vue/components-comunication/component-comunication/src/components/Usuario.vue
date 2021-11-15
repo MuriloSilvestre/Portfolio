4689d898fd68be9b287278f7a47bf58e56d06660
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-11-15 09:17:21 -0300

    comunication

--- vue/components-comunication/component-comunication/src/components/Usuario.vue
+++ vue/components-comunication/component-comunication/src/components/Usuario.vue
@@ -2,9 +2,13 @@
     <div class="container">
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
+        <p>Nome é: <strong>{{ nome }}</strong></p>
+        <button @click="alterarNome">Alterar Nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info />
+            <app-usuario-info :nome="nome"
+            @reiniciarNome=" nome = $event" 
+            :reiniciarFn="reiniciarNome"/>
             <app-usuario-editar />
         </div>
     </div>
@@ -15,7 +19,20 @@ import AppUsuarioInfo from './UsuarioInfo'
 import AppUsuarioEditar from './UsuarioEditar'
 
 export default {
-    components: { AppUsuarioInfo, AppUsuarioEditar }
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data:()=>{
+        return{
+            nome: "Murilo"
+        }
+    },
+    methods: {
+        alterarNome() {
+            this.nome = "Ana";
+        },
+        reiniciarNome(){
+            this.nome = "Murilo"
+        }
+    }
 }
 </script>
 
---
+++ vue/components-comunication/component-comunication/src/components/UsuarioInfo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverteNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn">Reiniciar Nome (CallBack)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        nome: {
+            type: String,
+            // required: true,
+            // default: ()=>{
+            //     return Array(10).fill(0).join(',')
+            // },
+            default: 'Anônimo'
+        },
+        reiniciarFn: Function
+    },
+    methods: {
+        inverteNome(){
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome(){
+            this.nome = "Murilo"
+            this.$emit('reiniciarNome', this.nome)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
