ac6e3ec5675425145adffd6c49648f19637557ae
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-11-16 11:28:01 -0300

    desafio

--- vue/components-comunication/component-comunication/src/components/UsuarioInfo.vue
+++ vue/components-comunication/component-comunication/src/components/UsuarioInfo.vue
@@ -3,12 +3,15 @@
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do Usuário: <strong>{{ inverteNome() }}</strong></p>
+        <p>Idade usuario: <strong>{{ idade }}</strong></p>
         <button @click="reiniciarNome">Reiniciar Nome</button>
         <button @click="reiniciarFn">Reiniciar Nome (CallBack)</button>
     </div>
 </template>
 
 <script>
+import barramento from '@/barramento'
+
 export default {
     props:{
         nome: {
@@ -19,7 +22,8 @@ export default {
             // },
             default: 'Anônimo'
         },
-        reiniciarFn: Function
+        reiniciarFn: Function,
+        idade: Number
     },
     methods: {
         inverteNome(){
@@ -29,6 +33,11 @@ export default {
             this.nome = "Murilo"
             this.$emit('reiniciarNome', this.nome)
         }
+    },
+    created(){
+        barramento.quandoIdadeMudar((idade) =>{
+            this.idade = idade
+        })
     }
 }
 </script>
