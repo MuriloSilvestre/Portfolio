d9870af12410e25b63ec9174cfc1443c4acbb93e
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-11-16 12:26:01 -0300

    desafio completo

---
+++ vue/components-comunication/comunicacao-desafio/src/barramento.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+export default new Vue({
+    methods:{
+        setUsuarioSelecionado(usuario){
+            this.$emit('setUsuarioSelecionado', usuario)
+        },
+        onUsuarioSelecionado(callback){
+            this.$on('setUsuarioSelecionado', callback)
+        }
+    }
+}
+)
