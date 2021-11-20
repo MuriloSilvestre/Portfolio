933befc19f529cb6bb8fd0d6cf3d2cdab676be36
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-11-20 07:54:54 -0300

    Filters-Mixins

---
+++ vue/Filters-Mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<input type="text" v-model ="cpfUsuario">
+		<p>{{ cpfUsuario | cpf }}</p>
+		<p>{{ cpfUsuario | cpf | inverter }}</p>
+		<hr>
+		<frutas />
+		<hr>
+		<div>
+			<ul>
+				<li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+			</ul>
+			<input type="text" v-model="fruta" @keydown.enter="add">
+    	</div>
+	</div>
+</template>
+
+<script>
+import Frutas from './Frutas.vue'
+import frutasMixin from './FrutasMixin'
+
+export default {
+	components: { Frutas },
+	mixins: [ frutasMixin ],
+	filters: {
+		cpf(valor){
+			const arr = valor.split('')
+			arr.splice(3,0,'.')
+			arr.splice(7,0,'.')
+			arr.splice(11,0,'-')
+			return arr.join('')
+		}
+	},
+	data:()=>{
+		return{
+			cpfUsuario:"60070080090",
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
