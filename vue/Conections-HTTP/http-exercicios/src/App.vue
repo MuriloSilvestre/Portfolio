bd61449ee83b747756c303fe58690aeb0873da12
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-11-27 12:06:50 -0300

    http

--- vue/Conections-HTTP/http-exercicios/src/App.vue
+++ vue/Conections-HTTP/http-exercicios/src/App.vue
@@ -2,19 +2,33 @@
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
 		<b-card>
-			<b-for-group label="Name:">
+			<b-form-group label="Name:">
 				<b-form-input type="text" size="lg"
 				v-model="usuario.name" placeholder="Informe o nome"></b-form-input>
-			</b-for-group>
-			<b-for-group label="Age:">
+			</b-form-group>
+			<b-form-group label="Age:">
 				<b-form-input type="number" size="lg"
 				v-model="usuario.age" placeholder="Informe a idade"></b-form-input>
-			</b-for-group>
-			<b-for-group label="E-Mail:">
-				<b-form-input type="text" size="lg"
+			</b-form-group>
+			<b-form-group label="E-Mail:">
+				<b-form-input type="email" size="lg"
 				v-model="usuario.email" placeholder="Informe o e-mail"></b-form-input>
-			</b-for-group>
+			</b-form-group>
+			<hr>
+			<b-button @click="salvar"
+				size="lg" variant="primary">Salvar</b-button>
+			<b-button @click="obterUsuarios"
+				size="lg" class="ml-2" variant="success">Obter Usuarios</b-button>
 		</b-card>
+		<hr>
+		<b-list-group>
+			<b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
+				<strong> Nome: </strong> {{usuario.name}}<br>
+				<strong> Idade: </strong> {{usuario.age}}<br>
+				<strong> E-mail: </strong> {{usuario.email}}<br>
+				<strong> Id: </strong> {{ id }}<br>
+			</b-list-group-item>
+		</b-list-group>
 	</div>
 </template>
 
@@ -22,20 +36,29 @@
 export default {
 	data:()=>{
 		return{
+			usuarios:[],
 			usuario:{
 				name:'',
 				age:0,
 				email:'',
 			}
 		}
+	},
+	methods:{
+		salvar(){
+			this.$http.post('usuarios.json', this.usuario)
+				.then(res =>{
+					this.usuario.name = ''
+					this.usuario.age = 0
+					this.usuario.email =''
+				})
+		},
+		obterUsuarios(){
+			this.$http.get('usuarios.json').then(res=>{
+				this.usuarios = res.data
+			})
+		}
 	}
-	// created(){
-	// 	this.$http.post('usuarios.json', {
-	// 		name: 'Murilo Silvestre',
-	// 		age: 26,
-	// 		email: 'm.serratosilvestre@gmail.com'
-	// 	}).then(res => console.log(res))
-	// }
 }
 </script>
 
