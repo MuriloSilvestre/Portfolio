8a95474949672b1793b052a79b44ec2a649ffed6
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-12-07 22:47:42 -0300

    Project Three

---
+++ vue/Project-Three-Stock-Trader/src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+	<v-toolbar app>
+		<v-toobar-title class="headline text-uppercase mr-4">
+			<span>STock</span>
+			<span class="font-weight-light">Trader</span>
+		</v-toobar-title>
+		<v-toolbar-items>
+			<v-btn flat to="/">Início</v-btn>
+			<v-btn flat to="/portfolio">Portfólio</v-btn>
+			<v-btn flat to="/stocks">Ações</v-btn>
+		</v-toolbar-items>
+		
+		<v-spacer></v-spacer>
+
+		<v-toolbar-items>
+			<v-btn flat >Finalizar Dia</v-btn>
+			<v-menu offset-y>
+				<v-btn flat slot="activator">Salvar & Carregar</v-btn>
+				<v-list>
+					<v-list-tile>
+						<v-list-tile-title>Salvar Dados</v-list-tile-title>
+					</v-list-tile>
+					<v-list-tile>
+						<v-list-tile-title>Carregar Dados</v-list-tile-title>
+					</v-list-tile>
+				</v-list>
+			</v-menu>
+		</v-toolbar-items>
+	</v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
