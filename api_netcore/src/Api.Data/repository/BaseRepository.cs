b012fd3d94547fb6cd89e924bd64b3a411bcb719
Murilo Silvestre <m.serratosilvestre@gmail.com>
2021-12-30 10:43:26 -0300

    Generic Methods Finish

--- api_netcore/src/Api.Data/repository/BaseRepository.cs
+++ api_netcore/src/Api.Data/repository/BaseRepository.cs
@@ -12,53 +12,98 @@ namespace Api.Data.repository
     {
         protected readonly MyContext _context;
         private DbSet<T> _dataset;
-        public BaseRepository(MyContext context){
+        public BaseRepository(MyContext context)
+        {
             _context = context;
             _dataset = _context.Set<T>();
         }
-        Task<bool> IRepository<T>.DeleteAsync(Guid id){
-            throw new NotImplementedException();
+        async Task<bool> IRepository<T>.DeleteAsync(Guid id)
+        {
+            try
+            {
+                var result = await _dataset.SingleOrDefaultAsync(p => p.Id.Equals(id));
+                if (result == null)
+                {
+                    return false;
+                }
+                _dataset.Remove(result);
+
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        async Task<T> IRepository<T>.InsertAsync(T item) {
-            try {
-                 if(item.Id == Guid.Empty){
-                     item.Id = Guid.NewGuid();
-                 }
-                 item.CreateAt = DateTime.UtcNow;
-                 _dataset.Add(item);
+        async Task<T> IRepository<T>.InsertAsync(T item)
+        {
+            try
+            {
+                if (item.Id == Guid.Empty)
+                {
+                    item.Id = Guid.NewGuid();
+                }
+                item.CreateAt = DateTime.UtcNow;
+                _dataset.Add(item);
 
-                 await _context.SaveChangesAsync();
+                await _context.SaveChangesAsync();
             }
-            catch (Exception ex) {   
+            catch (Exception ex)
+            {
                 throw ex;
             }
             return item;
         }
 
-        Task<T> IRepository<T>.SelectAsync(Guid id)
+        async Task<bool> ExistAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await _dataset.AnyAsync(p => p.Id.Equals(id));
         }
 
-        Task<IEnumerable<T>> IRepository<T>.SelectAsync()
+        async Task<T> IRepository<T>.SelectAsync(Guid id)
         {
-            throw new NotImplementedException();
-        }
-
-        async Task<T> IRepository<T>.UpdateAsync(T item) {
-            try {
-                 var result = await _dataset.SingleOrDefaultAsync(p => p.Id.Equals(item.Id));
-                 if (result == null){
-                     return null;
-                 }
-                 item.UpdateAt = DateTime.UtcNow;
-                 item.CreateAt = result.CreateAt;
-
-                 _context.Entry(result).CurrentValues.SetValues(item);
-                 await _context.SaveChangesAsync();
+            try
+            {
+                 return await _dataset.SingleOrDefaultAsync (p => p.Id.Equals(id));
             }
-            catch (Exception ex) {   
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        async Task<IEnumerable<T>> IRepository<T>.SelectAsync()
+        {
+            try
+            {
+                return await _dataset.ToListAsync();
+            }
+            catch (Exception ex)
+            {   
+                throw ex;
+            }
+        }
+
+        async Task<T> IRepository<T>.UpdateAsync(T item)
+        {
+            try
+            {
+                var result = await _dataset.SingleOrDefaultAsync(p => p.Id.Equals(item.Id));
+                if (result == null)
+                {
+                    return null;
+                }
+                item.UpdateAt = DateTime.UtcNow;
+                item.CreateAt = result.CreateAt;
+
+                _context.Entry(result).CurrentValues.SetValues(item);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
                 throw ex;
             }
             return item;
