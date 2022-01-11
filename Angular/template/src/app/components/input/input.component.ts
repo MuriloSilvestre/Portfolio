3c761827e28bce0011ed5c2815656bc3034cda98
Murilo Silvestre <m.serratosilvestre@gmail.com>
2022-01-11 11:54:59 -0300

    start

--- Angular/template/src/app/components/input/input.component.ts
+++ Angular/template/src/app/components/input/input.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input',
-  templateUrl: './input.component.html',
+  templateUrl: "./input.component.html",
   styleUrls: ['./input.component.scss']
 })
 export class InputComponent implements OnInit {
+
+  @Input() label: string = ''
 
   constructor() { }
 
