9cfc4885d09f79c2f46a3648e53d1ed69aff9a60
Murilo Silvestre <m.serratosilvestre@gmail.com>
2022-03-05 10:04:31 -0300

    react

--- front-end/react/fundamentos-react/src/App.jsx
+++ front-end/react/fundamentos-react/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Primerio from './components/basics/Primeiro';
 import ComParametro from './components/basics/ComParametro';
+import Fragmento from "./components/basics/Fragmento";
 
 export default _ =>
     <div>
@@ -9,4 +10,5 @@ export default _ =>
         <ComParametro
             titulo="Segundo Componente"
             subtitulo="Super Legal!!" />
+        <Fragmento />
     </div>
--- front-end/react/fundamentos-react/src/components/basics/Fragmento.jsx
+++ front-end/react/fundamentos-react/src/components/basics/Fragmento.jsx
@@ -1,1 +1,10 @@
 import React from "react";
+
+export default () => {
+    return (
+        <React.Fragment>
+            <h2>Fragment</h2>
+            <p>Cuidado com esse erro!</p>
+        </React.Fragment>
+    )
+}
