6e2742525321fd5f7e53edd01941fa29c27470c0
Murilo Silvestre <m.serratosilvestre@gmail.com>
2022-03-05 11:16:24 -0300

    desafio

--- front-end/react/fundamentos-react/src/App.jsx
+++ front-end/react/fundamentos-react/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Primerio from './components/basics/Primeiro';
 import ComParametro from './components/basics/ComParametro';
 import Fragmento from "./components/basics/Fragmento";
+import Aleatorio from "./components/basics/Aleatorio";
 
 export default _ =>
     <div>
         <h1>Fundamentos React</h1>
+        <Aleatorioio min={1} max={60} />
         <Primerio />
         <ComParametro
             titulo="Segundo Componente"
