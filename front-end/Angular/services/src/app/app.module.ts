0004ba8c2f54c428fe419b684d7b9107fb644918
Murilo Silvestre <m.serratosilvestre@gmail.com>
2022-05-28 20:21:37 -0300

    .

--- front-end/Angular/services/src/app/app.module.ts
+++ front-end/Angular/services/src/app/app.module.ts
@@ -1,23 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { LoggingService } from "./logging.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
+import { AppComponent } from "./app.component";
+import { AccountComponent } from "./account/account.component";
+import { NewAccountComponent } from "./new-account/new-account.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [
+    LoggingService,
+    //pode ser usasado componente  ou no module
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
